b2b627186545ad8800003f3115ba2fe971ba4036
Fenix-LAB <fenixcr.123@gmail.com>
2024-12-22 17:25:18 -0600

    Refactor configuration management: replace old config.json with updated version and adjust ROUTE_PATH format

--- config/config.py
+++ config/config.py
@@ -13,21 +13,22 @@ class Config(BaseSettings):
     JWT_ALGORITHM: str = "HS256"
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     EXCLUDED_URLS: list[str] = ["/api/auth/login", "/docs", "/redoc", "/openapi.json"]
-    ROUTE_PATH: str = "app/v1/"
+    ROUTE_PATH: str = "/app/v1"
 
+with open("config.json") as f:
+        production_conf = json.load(f)
 
 class ProductionConfig(Config):
-    with open("config.json") as f:
-        production_conf = json.load(f)
+    
 
     DEBUG: bool = production_conf["prod"]["debug"]
     APP_HOST: str = production_conf["prod"]["app"]["host"]
-    APP_PORT: int = production_conf["prod"]["app"]["port"]
+    APP_PORT: str = production_conf["prod"]["app"]["port"]
     JWT_SECRET: str = production_conf["prod"]["jwt"]["secret_key"]
     JWT_ALGORITHM: str = production_conf["prod"]["jwt"]["algorithm"]
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = production_conf["prod"]["jwt"]["access_token_expire_minutes"]
-    WRITER_DB_URL: str = production_conf["prod"]["db"]["writer"]
-    READER_DB_URL: str = production_conf["prod"]["db"]["reader"]
+    # WRITER_DB_URL: str = production_conf["prod"]["db"]["writer"]
+    # READER_DB_URL: str = production_conf["prod"]["db"]["reader"]
     EXCLUDED_URLS: list[str] = production_conf["prod"]["excluded_urls"]
     ROUTE_PATH: str = production_conf["prod"]["route_path"]
 
