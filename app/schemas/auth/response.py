4b4af43a48f876f04bffb9d287a33ea94a9cf1aa
Fenix-LAB <fenixcr.123@gmail.com>
2025-01-06 12:46:10 -0600

    Refactor authentication schemas by consolidating request and response models into single files

--- app/schemas/auth/response.py
+++ app/schemas/auth/response.py
@@ -3,4 +3,3 @@ from pydantic import BaseModel, Field
 
 class LoginResponse(BaseModel):
     Token: str = Field(..., description="Token")
-    # refresh_token: str = Field(..., description="Refresh token")
