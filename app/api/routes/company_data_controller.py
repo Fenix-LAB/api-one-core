a4661dca2e4b608dbc5b98a526e91778ea213707
Fenix-LAB <fenixcr.123@gmail.com>
2025-02-09 16:35:27 -0600

    Refactor saveSocioAccionista endpoint and SocioAccionistaSaveRequest schema; update field names to lowercase, enhance response handling, and implement save_socio_accionista function for external API integration

--- app/api/routes/company_data_controller.py
+++ app/api/routes/company_data_controller.py
@@ -10,19 +10,7 @@ from app.middleware.authentication import BaseData
 from app.database.session import get_db_session
 
 from app.schemas.generic_response import ApiResponse
-from app.schemas.generic_list import ListResponse
 
-from app.schemas.company_data.response import (
-    SectionOptionDatosEmpresaResponse,
-    HistoricoResponse,
-    RazonSocialResponse,
-    DatosEmpresaEvidenciaResponse,
-    ClienteProveedorResponse,
-    ProveedorNacionalResponse,
-    SocioAccionistaResponse,
-    LegalUsoResponse,
-    EnlaceOperativoResponse,
-)
 from app.schemas.company_data.request import (
     SectionDatosEmpresaListRequest,
     RazonSocialHistoricoRequest,
@@ -47,17 +35,6 @@ from app.schemas.company_data.request import (
 
 from app.services.company_data.company_data_services import *
 
-from app.schemas.models import (
-    HallazgoOptionModel,
-    AreaRolModel,
-    DatosEmpresaSocioAccionistaCaracterModel,
-    PaisModel,
-    PaisEstadoModel,
-    DatosEmpresaLegalUsoModel,
-
-)
-
-from app.services.get_requirement_obligation import get_requerimiento_obligaciones
 from app.services.role_checker import RoleChecker, get_current_user
 
 from config.logger_config import logger
@@ -73,7 +50,6 @@ async def getSectionList(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -92,16 +68,6 @@ async def getSectionList(
     """
 
     logger.info(f"ENDPOINT /getSectionList: {request}")
-
-    # data = [
-    #     SectionOptionDatosEmpresaResponse(Code="RazonSocial", Cantidad=0, Total=11, Selected=False),
-    #     SectionOptionDatosEmpresaResponse(Code="Domicilio", Cantidad=0, Total=29, Selected=False),
-    #     SectionOptionDatosEmpresaResponse(Code="ClienteExtranjero", Cantidad=3, Total=15, Selected=False),
-    #     SectionOptionDatosEmpresaResponse(Code="ProveedorNacional", Cantidad=7, Total=10, Selected=False),
-    #     SectionOptionDatosEmpresaResponse(Code="SociosAccionistas", Cantidad=2, Total=8, Selected=False),
-    #     SectionOptionDatosEmpresaResponse(Code="LegalUso", Cantidad=9, Total=20, Selected=False),
-    #     SectionOptionDatosEmpresaResponse(Code="EnlacesOperativos", Cantidad=4, Total=4, Selected=False),
-    # ]
 
     try:
 
@@ -137,7 +103,6 @@ async def getRazonSocialHistoricoList(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -169,49 +134,6 @@ async def getRazonSocialHistoricoList(
     """
 
     logger.info(f"ENDPOINT /getRazonSocialHistoricoList: {request}")
-
-    # data = [
-    #     HistoricoResponse(
-    #         Status="Modificado",
-    #         Usuario="Usuario 1",
-    #         Fecha=datetime.utcnow(),
-    #         Data=RazonSocialResponse(
-    #             ID=1,
-    #             CaseNumber=1,
-    #             Name="Razón Social 1",
-    #             RFC="RFC 1",
-    #             Folio="Folio 1",
-    #             MovementDate=datetime.utcnow(),
-    #             DeedDate=datetime.utcnow(),
-    #             Fedatario="Fedatario 1",
-    #             Notary="Notario 1",
-    #             Effect="Estatutos",
-    #             Notice="Aviso 1",
-    #             NoticeDate=datetime.utcnow(),
-    #             IsCompany=True
-    #         )
-    #     ),
-    #     HistoricoResponse(
-    #         Status="Vigente",
-    #         Usuario="Usuario 2",
-    #         Fecha=datetime.utcnow(),
-    #         Data=RazonSocialResponse(
-    #             ID=2,
-    #             CaseNumber=2,
-    #             Name="Razón Social 2",
-    #             RFC="RFC 2",
-    #             Folio="Folio 2",
-    #             MovementDate=datetime.utcnow(),
-    #             DeedDate=datetime.utcnow(),
-    #             Fedatario="Fedatario 2",
-    #             Notary="Notario 2",
-    #             Effect="Fusion",
-    #             Notice="Aviso 2",
-    #             NoticeDate=datetime.utcnow(),
-    #             IsCompany=False
-    #         )
-    #     )
-    # ]
 
     try:
 
@@ -245,7 +167,6 @@ async def saveRazonSocial(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -276,22 +197,6 @@ async def saveRazonSocial(
     """
 
     logger.info(f"ENDPOINT /saveRazonSocial: {request}")
-
-    # data = RazonSocialResponse(
-    #     ID=1,
-    #     CaseNumber=1,
-    #     Name="Razón Social 1",
-    #     RFC="RFC 1",
-    #     Folio="Folio 1",
-    #     MovementDate="2023-01-01T00:00:00",
-    #     DeedDate="2023-01-02T00:00:00",
-    #     Fedatario="Fedatario 1",
-    #     Notary="Notario 1",
-    #     Effect="Estatutos",
-    #     Notice="Aviso 1",
-    #     NoticeDate="2023-01-03T00:00:00",
-    #     IsCompany=True,
-    # )
 
     try:
         response = await save_razon_social(
@@ -330,7 +235,6 @@ async def getHallazgosList(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -349,12 +253,6 @@ async def getHallazgosList(
     logger.info(f"ENDPOINT /getHallazgosList: {request}")
 
     try:
-        # data = [
-        #     HallazgoOptionModel(ID=1, Code="Code 1", Nombre="Hallazgo 01"),
-        #     HallazgoOptionModel(ID=2, Code="Code 2", Nombre="Hallazgo 02"),
-        #     HallazgoOptionModel(ID=3, Code="Code 3", Nombre="Hallazgo 03"),
-        #     HallazgoOptionModel(ID=4, Code="Code 4", Nombre="Hallazgo 04"),
-        # ]
 
         data, token = await fetch_hallazgos_list(
             code_section=request.CodeSection,
@@ -384,7 +282,6 @@ async def getEvidenciaID(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -425,16 +322,6 @@ async def getEvidenciaID(
     logger.info(f"ENDPOINT /getEvidenciaID: {request}")
 
     try:
-        # evidencia = DatosEmpresaEvidenciaResponse(
-        #     ID=request.ID,
-        #     CaseNumber=request.ID,
-        #     AreaRols=None,
-        #     Responsables=None,
-        #     Cliente="Traders",
-        #     Fecha=datetime.utcnow(),
-        #     Recomendaciones=None,
-        #     Hallazgo=None,
-        # )
 
         response, token = await fetch_evidencia_id(
             id=request.id,
@@ -466,7 +353,6 @@ async def saveEvidencia(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -534,7 +420,6 @@ async def getPaises(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -581,7 +466,6 @@ async def getPaisEstados(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -631,7 +515,6 @@ async def getClienteProveedorList(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -697,7 +580,6 @@ async def saveClienteProveedor(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -775,7 +657,6 @@ async def getProveedorNacionalList(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -830,7 +711,6 @@ async def saveProveedorNacional(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -867,14 +747,14 @@ async def saveProveedorNacional(
 
         if response.status_code == 200:
             return ApiResponse(
-                success=True,  # Corregido de "Status" a "Success"
+                success=True,
                 message="OK",
                 data=True,
                 token=user_data.token,
             )
 
         return ApiResponse(
-            success=True,  # Corregido de "Status" a "Success"
+            success=True,
             message="OK",
             data=True,
             token=user_data.token,
@@ -882,7 +762,7 @@ async def saveProveedorNacional(
     except Exception as e:
         logger.error(f"ENDPOINT /saveProveedorNacional: {str(e)}")
         return ApiResponse(
-            success=False,  # Corregido de "Status" a "Success"
+            success=False,
             message="Se presentó un error",
             data=False,
             token=user_data.token,
@@ -896,7 +776,6 @@ async def getCaracterTipos(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -943,7 +822,6 @@ async def getSocioAccionistaList(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -1004,7 +882,6 @@ async def saveSocioAccionista(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -1076,7 +953,6 @@ async def getLegalUsoList(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -1131,85 +1007,11 @@ async def getLegalUsoList(
     logger.info(f"ENDPOINT /getLegalUsoList: {request}")
 
     try:
-        # data = [
-        #     HistoricoResponse(
-        #         Status="Modificado",
-        #         Usuario="Usuario 1",
-        #         Fecha="2024-01-01T12:00:00",
-        #         Data=LegalUsoResponse(
-        #             ID=1,
-        #             CaseNumber=1,
-        #             DomicilioAcreditacion=DatosEmpresaLegalUsoModel(
-        #                 Calle="Calle 1",
-        #                 Colonia="Colonia 1",
-        #                 CP="12345",
-        #                 Municipio="Municipio 1",
-        #                 EstadoCode="cod1",
-        #                 EstadoNombre="Estado 1",
-        #                 PaisCode="MX",
-        #                 Localidad="Localidad 1",
-        #                 NumeroExterior="Numero Exterior 1",
-        #                 NumeroInterior="Numero Interior 1",
-        #                 FechaInicioVigencia="2024-01-01T12:00:00",
-        #             ),
-        #             DomicilioNuevo=DatosEmpresaLegalUsoModel(
-        #                 Calle="Nuevo Calle 1",
-        #                 Colonia="Nuevo Colonia 1",
-        #                 CP="123",
-        #                 Municipio="Nuevo Municipio 1",
-        #                 EstadoCode="cod2",
-        #                 EstadoNombre="Estado 2",
-        #                 PaisCode="CO",
-        #                 Localidad="Nuevo Localidad 1",
-        #                 NumeroExterior="Nuevo Numero Exterior 1",
-        #                 FechaInicioVigencia="2024-01-01T12:00:00",
-        #                 TipoDocumento=2
-        #             )
-        #         )
-        #     ),
-        #     HistoricoResponse(
-        #         Status="Modificado",
-        #         Usuario="Usuario 2",
-        #         Fecha="2024-01-02T12:00:00",
-        #         Data=LegalUsoResponse(
-        #             ID=2,
-        #             CaseNumber=3,
-        #             DomicilioAcreditacion=DatosEmpresaLegalUsoModel(
-        #                 Calle="Calle 2",
-        #                 Colonia="Colonia 2",
-        #                 CP="12345",
-        #                 Municipio="Municipio 2",
-        #                 EstadoCode="cod2",
-        #                 EstadoNombre="Estado 2",
-        #                 PaisCode="MX",
-        #                 Localidad="Localidad 2",
-        #                 NumeroExterior="Numero Exterior 2",
-        #                 NumeroInterior="Numero Interior 2",
-        #                 FechaInicioVigencia="2024-01-02T12:00:00",
-        #             ),
-        #             DomicilioNuevo=DatosEmpresaLegalUsoModel(
-        #                 Calle="Nuevo Calle 2",
-        #                 Colonia="Nuevo Colonia 2",
-        #                 CP="1232",
-        #                 Municipio="Nuevo Municipio 2",
-        #                 EstadoCode="cod3",
-        #                 EstadoNombre="Estado 2",
-        #                 PaisCode="US",
-        #                 Localidad="Nuevo Localidad 2",
-        #                 NumeroExterior="Nuevo Numero Exterior 2",
-        #                 NumeroInterior="Nuevo Numero Interior 2",
-        #                 FechaInicioVigencia="2024-01-02T12:00:00",
-        #                 TipoDocumento=4
-        #             )
-        #         )
-        #     )
-        # ]
 
         data, token = await fetch_legal_uso_list(
             token=user_data.token,
         )
     
-
         return ApiResponse(
             success=True,
             message="OK",
@@ -1234,7 +1036,6 @@ async def saveLegalUso(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -1329,7 +1130,6 @@ async def getEnlacesOperativosList(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -1343,39 +1143,6 @@ async def getEnlacesOperativosList(
     - CaseNumber
 
     """
-
-    # data = [
-    #     HistoricoResponse(
-    #         Status="Modificado",
-    #         Usuario="Usuario 1",
-    #         Fecha="2025-01-07T00:00:00",
-    #         Data=EnlaceOperativoResponse(
-    #             ID=1,
-    #             CaseNumber=1,
-    #             Nombre="Nombre 1",
-    #             RFC="RFC 1",
-    #             TipoRelacion="Relación 1",
-    #             FechaInicio="2025-01-07T00:00:00",
-    #             FechaFin="2025-01-07T00:00:00",
-    #             Observaciones="Observaciones 1",
-    #         ),
-    #     ),
-    #     HistoricoResponse(
-    #         Status="Modificado",
-    #         Usuario="Usuario 2",
-    #         Fecha="2025-01-07T00:00:00",
-    #         Data=EnlaceOperativoResponse(
-    #             ID=2,
-    #             CaseNumber=2,
-    #             Nombre="Nombre 2",
-    #             RFC="RFC 2",
-    #             TipoRelacion="Relación 2",
-    #             FechaInicio="2025-01-07T00:00:00",
-    #             FechaFin="2025-01-07T00:00:00",
-    #             Observaciones="Observaciones 2",
-    #         ),
-    #     ),
-    # ]
 
     try:
 
@@ -1402,7 +1169,6 @@ async def getEnlacesOperativosList(
         )
 
 
-
 @app.post("/saveEnlaceOperativo")
 @inject
 async def saveEnlaceOperativo(
@@ -1410,7 +1176,6 @@ async def saveEnlaceOperativo(
     _: RoleChecker = Depends(RoleChecker(allowed_roles=["Admin"])),
     credentials: HTTPAuthorizationCredentials = Depends(security),
     user_data: BaseData = Depends(get_current_user),
-    db_session: AsyncSession = Depends(get_db_session),
 ):
     """
     ## DESCRIPTION
@@ -1461,44 +1226,3 @@ async def saveEnlaceOperativo(
             data=None,
             token=None,
         )
-
-
-# @app.post("/saveEnlaceOperativo")
-# @inject
-# async def saveEnlaceOperativo(
-#     _: RoleChecker = Depends(RoleChecker(allowed_roles=["admin"])),
-#     credentials: HTTPAuthorizationCredentials = Depends(security),
-#     user_data: BaseData = Depends(get_current_user),
-#     db_session: AsyncSession = Depends(get_db_session),
-# ):
-#     """
-#     ## DESCRIPTION
-#     ### Endpoint to save an operative link.
-
-#     ## REQUEST
-#     - ID
-
-#     ## RESPONSE
-#     - 200: Success
-#     - 400: Bad Request
-#     - 401: Unauthorized
-#     - 403: Forbidden
-#     - 500: Internal Server Error
-
-#     """
-
-#     try:
-#         return ApiResponse(
-#             Success=True,
-#             Message="OK",
-#             Data=True,
-#             Token=user_data.token,
-#         )
-#     except Exception as e:
-#         logger.error(f"ENDPOINT /saveEnlaceOperativo: {e}")
-#         return ApiResponse(
-#             Success=False,
-#             Message="Se presentó un error",
-#             Data=False,
-#             Token=user_data.token,
-#         )
